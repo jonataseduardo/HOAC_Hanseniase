693c3364d0887fe70f2bf8a93d52051f135bed58
jonatas <jonataseduardo@gmail.com>
2022-03-15 12:08:58 -0300

    r2beta

--- src/quality.R
+++ src/quality.R
@@ -1,10 +1,8 @@
 library(knitr)
 library(performance)
 library(ggplot2)
-#library(multilevelTools)
-#mitl::multilevelR2
-#install.packages('partR2')
-#library('partR2')
+library(lmerTest)
+library(r2glmm)
 
 source("statistics_utils.R")
 
@@ -78,6 +76,28 @@ lg_anova <-
 
 
 
+group_r2beta <- 
+  function(model_list)
+    rbindlist(
+      lapply(
+        model_list, 
+        function(m){
+          data.table(r2beta(m, partial=T))[1,6:8]
+        }
+        ), 
+      idcol = "grupo", 
+      use.names = TRUE
+    )
+
+lg_r2beta <-
+  function(l, g){
+    fit_list <- anlz_pipeline(data, l, g)[[3]]
+    res <- group_r2beta(fit_list)
+    res[, modelo := l]
+    return(res)
+  }
+
+
 ### Main ####
 
 data <- read_and_preparedata()
@@ -86,10 +106,10 @@ level_list <- c('uf', 'macro')
 group_list <- c('total', 'sexo', 'idade')
 
 
-l <- level_list[1]
-g <- group_list[1]
-i <- 1
-m <- anlz_pipeline(data, l, g)[[3]][[i]]
+#l <- level_list[1]
+#g <- group_list[1]
+#i <- 1
+#m <- anlz_pipeline(data, l, g)[[3]][[i]]
 
 
 for(l in level_list){
@@ -127,7 +147,24 @@ performance_results <-
       }
     ), fill = TRUE, use.names = TRUE)
 
-performance_results
+performance_results <-  
+  performance_results[, .SD, by = .(modelo, grupo)]
+
+
+r2beta_results <- 
+  rbindlist(lapply(
+    level_list, 
+    function(l){
+      rbindlist(lapply(
+        group_list, 
+        function(g) {lg_r2beta(l,g)}
+        ))
+      }
+    ), fill = TRUE, use.names = TRUE)
+
+
+r2beta_results <-  
+  r2beta_results[, .SD, by = .(modelo, grupo)]
 
 
 effects_results <- 
@@ -141,6 +178,9 @@ effects_results <-
       }
     ), fill = TRUE, use.names = TRUE)
 
+effects_results <-  
+  effects_results[, .SD, by = c("modelo", "grupo", "Fixed Effect")]
+
 
 anova_results <- 
   rbindlist(lapply(
@@ -153,6 +193,11 @@ anova_results <-
       }
     ), fill = TRUE, use.names = TRUE)
 
+anova_results <-  
+  anova_results[, .SD, by = .(modelo, grupo)]
 
 
-
+fwrite(performance_results, '../data/perfomance.csv')
+fwrite(r2beta_results, '../data/r2beta.csv')
+fwrite(effects_results, '../data/effects.csv')
+fwrite(anova_results, '../data/anova.csv')
